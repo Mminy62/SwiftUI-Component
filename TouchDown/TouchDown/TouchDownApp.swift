2c62a03dc73bb80addcb6890f8fee08b17552ba3
Minyoung Lee <mminy62@gmail.com>
2024-01-30 03:00:44 +0900

    [Init] 이미지 추가 및 초기 세팅

---
+++ TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 이민영 on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
