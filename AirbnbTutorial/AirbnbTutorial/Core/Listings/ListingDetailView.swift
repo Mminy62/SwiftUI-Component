ffed26a4e9263600b30ed86ad45bb485e6de0998
Minyoung Lee <mminy62@gmail.com>
2024-10-09 16:54:43 +0900

    Feat(Listings): ListDetailView UI 구현

---
+++ AirbnbTutorial/AirbnbTutorial/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,220 @@
+//
+//  ListingDetailView.swift
+//  AirbnbTutorial
+//
+//  Created by 이민영 on 10/9/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ListingDetailView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .topLeading) {
+                ListingImageCarouselView()
+                    .frame(height: 320)
+                
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundStyle(.black)
+                        .background {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 32, height: 32)
+                        }
+                        .padding(32) // label에 padding을 해야 클릭이 된다.
+                    // button에 padding을 넣으면 클릭 안됨
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                VStack(alignment: .leading) {
+                    HStack(spacing: 2) { // refactor
+                        Image(systemName: "star.fill")
+                        
+                        Text("4.86")
+                        
+                        Text(" - ")
+                        
+                        Text("28 reviews")
+                            .underline()
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.black)
+                    
+                    Text("Miami Florida")
+                }
+                .font(.caption)
+            }
+            .padding(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Divider()
+            
+            // host info view
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Entire villa hosted by John Smith")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack {
+                        Text("4 guests - ")
+                        Text("4 bedrooms - ")
+                        Text("4 beds - ")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("male-profile-photo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing features
+            VStack(alignment: .leading, spacing: 16) {
+                ForEach(0 ..< 2) {feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            Text("Superhosts are experience, highly rated hosts who are commited to providing great start fot guests")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            // bedrooms view
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 5) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width: 132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing amenities
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What this place offers")
+                    .font(.headline)
+                
+                ForEach(0 ..< 5) { feature in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("Wifi")
+                            .font(.footnote)
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            // map view
+            VStack(alignment: .leading) {
+                Text("Where you'll be")
+                    .font(.headline)
+                
+                Map()
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .ignoresSafeArea()
+        .padding(.bottom, 64) // overlay로 가려지는 content를 위한 높이 생성
+        .overlay(alignment: .bottom) {
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("$500")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("Total before taxes")
+                        
+                        Text("Oct 15 - 20")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                }
+                .padding(.horizontal, 32)
+            }
+            .background(.white)
+        }
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
