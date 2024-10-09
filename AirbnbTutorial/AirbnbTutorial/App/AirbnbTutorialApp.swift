6a18a322ec881c6710c412532be97811b14ccd75
Minyoung Lee <mminy62@gmail.com>
2024-10-09 11:20:19 +0900

    Init: AirbnbTutorial 프로젝트 생성

---
+++ AirbnbTutorial/AirbnbTutorial/App/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by 이민영 on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
